16ce62ac4c4deacb7febb5da692f5c8f82b6b969
lolmoon1 <46765900+lolmoon1@users.noreply.github.com>
2025-05-18 23:04:31 -0500

    Initial Commit
    Added the json.lua library (by rxi),
    Initial infrastructure
    accounts.json file, MEANT TO BE INSECURE,
    added accountManger.lua

---
+++ accountManager.lua
@@ -0,0 +1,8 @@
+-- Manages the Accounts from accounts.json File
+-- Format = {Accounts : {"IGN" : [accountNumber, pin, balance]}}
+
+local json = require("lib/json")
+
+local jsonStorage = {}
+
+print("THE CODE WORKSSSSS")
