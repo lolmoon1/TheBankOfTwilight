e133600dec2c844ec2dc1b45ba339b6370ad6f1e
lolmoon1 <46765900+lolmoon1@users.noreply.github.com>
2025-05-19 10:27:53 -0500

    Update accountManager.lua

--- accountManager.lua
+++ accountManager.lua
@@ -2,12 +2,14 @@
 -- Format = {Accounts : {"IGN" : [accountNumber, pin, balance]}}
 
 local json = require("lib/json")
-local io = require("io")
 
 local accounts = {}
 
 --opens the json file and formats it to a lua table (thanks rxi), then places it into jsonStorage
-local jsonStorage = io.open("accounts.json", "r")
+local jsonStorage = io.open("./accounts.json", "r")
+if not jsonStorage then
+	error("Error: Unable to open accounts.json. Please ensure the file exists and is accessible.")
+end
 local accounts = json.decode(jsonStorage:read("*all"))
 jsonStorage:close()
 
